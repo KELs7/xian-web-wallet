dbf9dfba6a46f8da7bd6da33cebd167068a9f4f8
kels7 <hiketabo@gmail.com>
2025-04-25 13:26:04 +0000

    fix: update with new variable

--- js/extension.js
+++ js/extension.js
@@ -27,7 +27,7 @@ if (runningAsExtension()) {
     // Listen for messages from the background script
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (message.type === 'getWalletInfo') {
-            sendResponse({address: publicKey, locked: locked, chainId: CHAIN_ID});
+            sendResponse({address: selectedAccountVk, locked: locked, chainId: CHAIN_ID});
         }
         if (message.type === 'dAppSendTransaction') {
             if (locked) {
